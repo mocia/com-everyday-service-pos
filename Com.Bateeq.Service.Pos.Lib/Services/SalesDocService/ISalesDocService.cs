5f0a7f789ebf4cea85512495e4f97395a92e4d5b
ardana96 <ardanagama@gmail.com>
2020-10-28 17:10:16 +0700

    Omzet Daily, omzet report

--- Com.Bateeq.Service.Pos.Lib/Services/SalesDocService/ISalesDocService.cs
+++ Com.Bateeq.Service.Pos.Lib/Services/SalesDocService/ISalesDocService.cs
@@ -2,6 +2,7 @@
 using Com.Bateeq.Service.Pos.Lib.ViewModels.SalesDoc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,5 +20,10 @@ namespace Com.Bateeq.Service.Pos.Lib.Services.SalesDocService
         SalesDoc ReadModelByCode(string code, string storecode);
         Tuple<List<SalesDoc>, int, Dictionary<string, string>, List<string>> ReadModelReturn(string storecode, int Page = 1, int Size = 25, string Order = "{}", string Keyword = null, string Filter = "{}", string Username = "");
         Tuple<List<PaymentMethodReportViewModel>, int> GetPaymentMethodReport(string storecode, DateTimeOffset dateFrom, DateTimeOffset dateTo, string shift, string info, int offset, string username, int page, int size, string Order = "{}");
+
+        //Tuple<List<OmzetReportViewModel>, int> GetOmzetReport(string storecode, DateTimeOffset dateFrom, DateTimeOffset dateTo, string shift, string info, int offset, string username, int page = 1, int size = 25, string Order = "{}");
+        Tuple<TotalCategoryViewModel, int> GetOmzetDailyReport(DateTimeOffset dateFrom, DateTimeOffset dateTo, int offset, int page = 1, int size = 25, string Order = "{}");
+        List<SalesDoc> OmzetReport(string storecode, DateTimeOffset dateFrom, DateTimeOffset dateTo, string shift);
+        MemoryStream GenerateExcelOmzet(string storecode, DateTimeOffset dateFrom, DateTimeOffset dateTo, string shift);
     }
 }
---
+++ Com.Bateeq.Service.Pos.Lib/ViewModels/SalesDoc/OmzetDailyReportViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Bateeq.Service.Pos.Lib.ViewModels.SalesDoc
+{
+    public class OmzetDailyReportViewModel
+    {
+    }
+
+    public class StoreViewModel
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string OnlineOffline { get; set; }
+        public string SalesCategory { get; set; }
+        public string StoreCategory { get; set; }
+        public string Status { get; set; }
+        public string Remark { get; set; }
+
+
+    }
+
+
+    public class CategoryViewModel
+    {
+        public string CategoryName { get; set; }
+        public string OfflineOnline { get; set; }
+        public double GrandTotal { get; set; }
+        public double Count { get; set; }
+    }
+
+    public class CategoryCollectViewModel
+    {
+        public CategoryViewModel StandAlone { get; set; }
+        public CategoryViewModel Konsinyasi { get; set; }
+        public CategoryViewModel Online { get; set; }
+        public CategoryViewModel WholeSale { get; set; }
+        public CategoryViewModel vvip { get; set; }
+    }
+
+
+    public class DataViewModel
+    {
+        public List<TotalViewModel> StandAlone { get; set; }
+        public List<TotalViewModel> Konsinyasi { get; set; }
+        public List<TotalViewModel> Online { get; set; }
+        public List<TotalViewModel> WholeSale { get; set; }
+        public List<TotalViewModel> vvip { get; set; }
+    }
+
+    public class TotalViewModel
+    {
+        public double GrandTotal { get; set; }
+        public double Count { get; set; }
+        public StoreViewModel Store { get; set; }
+    }
+
+    public class TotalCategoryViewModel
+    {
+        public CategoryCollectViewModel CategoryList { get; set; }
+        public DataViewModel DataList { get; set; }
+    }
+}

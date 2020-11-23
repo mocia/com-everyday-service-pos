9f35ce4707fb3b5d233fb587e23e21ea182563f6
yokana777 <kawaiirockmansick@gmail.com>
2020-11-23 13:55:24 +0700

    update report
    sales return + sales void

--- Com.Bateeq.Service.Pos.Lib/Services/SalesDocService/ISalesDocService.cs
+++ Com.Bateeq.Service.Pos.Lib/Services/SalesDocService/ISalesDocService.cs
@@ -1,4 +1,5 @@
 ﻿using Com.Bateeq.Service.Pos.Lib.Models.SalesDoc;
+using Com.Bateeq.Service.Pos.Lib.ViewModels;
 using Com.Bateeq.Service.Pos.Lib.ViewModels.SalesDoc;
 using System;
 using System.Collections.Generic;
@@ -20,7 +21,7 @@ namespace Com.Bateeq.Service.Pos.Lib.Services.SalesDocService
         SalesDoc ReadModelByCode(string code, string storecode);
         Tuple<List<SalesDoc>, int, Dictionary<string, string>, List<string>> ReadModelReturn(string storecode, int Page = 1, int Size = 25, string Order = "{}", string Keyword = null, string Filter = "{}", string Username = "");
         Tuple<List<PaymentMethodReportViewModel>, int> GetPaymentMethodReport(string storecode, DateTimeOffset dateFrom, DateTimeOffset dateTo, string shift, string info, int offset, string username, int page, int size, string Order = "{}");
-
+        Tuple<List<SalesVoidReportViewModel>, int> GetSalesVoidReport(string storeCode, DateTimeOffset dateFrom, DateTimeOffset dateTo, string shift, int offset, int page, int size, string Order = "{}");
         //Tuple<List<OmzetReportViewModel>, int> GetOmzetReport(string storecode, DateTimeOffset dateFrom, DateTimeOffset dateTo, string shift, string info, int offset, string username, int page = 1, int size = 25, string Order = "{}");
         Tuple<TotalCategoryViewModel, int> GetOmzetDailyReport(DateTimeOffset dateFrom, DateTimeOffset dateTo, int offset, int page = 1, int size = 25, string Order = "{}");
         List<SalesDoc> OmzetReport(string storecode, DateTimeOffset dateFrom, DateTimeOffset dateTo, string shift);

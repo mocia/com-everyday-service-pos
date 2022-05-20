2a15b4621623a16ce89465fe44fd468840a3d042
achensetyawati <setyawati@moonlay.com>
2022-05-20 09:45:30 +0700

    salesdoc unit test

---
+++ Com.Everyday.Service.Pos.Test/DataUtil/SalesDocDataUtils/SalesDocDataUtil.cs
@@ -0,0 +1,67 @@
+﻿using Com.Everyday.Service.Pos.Lib.Models.SalesDoc;
+using Com.Everyday.Service.Pos.Lib.Services.SalesDocService;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.Everyday.Service.Pos.Test.DataUtil.SalesDocDataUtils
+{
+    public class SalesDocDataUtil
+    {
+        private readonly SalesDocService _service;
+        public SalesDocDataUtil(SalesDocService service)
+        {
+            _service = service;
+        }
+
+
+        public SalesDoc GetNewData()
+        {
+            SalesDoc TestData = new SalesDoc()
+            {
+                Code = "code",
+                BankCardName = "name",
+                PaymentType = "Cash",
+                Date = DateTimeOffset.UtcNow,
+                StoreId = 1,
+                StoreStorageId = 1,
+                StoreStorageCode = "code",
+                StoreStorageName = "Name",
+                StoreCategory = "offline",
+                StoreCode = "code",
+                BankCardId = 1,
+                BankCardCode = "USD",
+                VoucherValue = 1,
+                BankId = 1,
+                BankName = "BankName",
+                BankCode = "BankCode",
+                StoreName= "name",
+                Details = new List<SalesDocDetail>()
+                {
+                    new SalesDocDetail()
+                    {
+                        ItemCode = "code",
+                        ItemId = 1,
+                        isReturn=false,
+                        ItemName="name",
+                        ItemDomesticCOGS=1,
+
+                    },
+
+                }
+            };
+
+            return TestData;
+        }
+
+
+
+        public async Task<SalesDoc> GetTestData()
+        {
+            var data = GetNewData();
+            await _service.Create(data);
+            return _service.ReadModelById(data.Id);
+        }
+    }
+}
